79c6d1e5ac2a7cd5679c3685797a4d2a1e91f465
unknown <aviralkakkar3@gmil.com>
2021-03-04 11:20:41 +0530

    commit

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var jsonRes;
 var SourceListDEResult;
 var xml2js = require('xml2js');
 var xml2jsParser = new xml2js.Parser();
+var DEListMap={};
+var favorites = [];
 
 // use the express-static middleware
 app.use(express.static("marketing-cloud-query-app"));
@@ -109,7 +111,7 @@ app.post("/secondpage", async function (req, res) {
 
    app.post("/fields", async (reqCall,resCall)=>
    {
-    console.log("yeh app.post fields wale me hai token" + access_token);
+    console.log("yeh app.post fields wale me hai token " + access_token);
     var body2='<?xml version="1.0" encoding="UTF-8"?>\r\n<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">\r\n    <s:Header>\r\n        <a:Action s:mustUnderstand="1">Retrieve</a:Action>\r\n        <a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID>\r\n        <a:ReplyTo>\r\n            <a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address>\r\n        </a:ReplyTo>\r\n        <a:To s:mustUnderstand="1">https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx</a:To>\r\n        <fueloauth xmlns="http://exacttarget.com">' + access_token + '</fueloauth>\r\n    </s:Header>\r\n    <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\r\n        <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI">\r\n            <RetrieveRequest>\r\n                <ObjectType>DataExtensionField</ObjectType>\r\n                <Properties>Client.ID</Properties>\r\n                <Properties>CreatedDate</Properties>\r\n                <Properties>CustomerKey</Properties>\r\n                <Properties>DataExtension.CustomerKey</Properties>\r\n                <Properties>DefaultValue</Properties>\r\n                <Properties>FieldType</Properties>\r\n                <Properties>IsPrimaryKey</Properties>\r\n                <Properties>IsRequired</Properties>\r\n                <Properties>MaxLength</Properties>\r\n                <Properties>ModifiedDate</Properties>\r\n                <Properties>Name</Properties>\r\n                <Properties>ObjectID</Properties>\r\n                <Properties>Ordinal</Properties>\r\n                <Properties>Scale</Properties>\r\n\r\n                               <QueryAllAccounts>true</QueryAllAccounts>\r\n                <Retrieves />\r\n                <Options>\r\n                    <SaveOptions />\r\n                    <IncludeObjects>true</IncludeObjects>\r\n                </Options>\r\n            </RetrieveRequest>\r\n      </RetrieveRequestMsg>\r\n   </s:Body>\r\n</s:Envelope>'
     var options = {
       'method': 'POST',
@@ -125,29 +127,52 @@ app.post("/secondpage", async function (req, res) {
       if (error) throw new Error(error);
        
       SourceDEFieldsResult = response.body;
+     
 
         xml2jsParser.parseString(SourceDEFieldsResult, function (err, result) {
-          //console.log('mera result : ' + JSON.stringify(result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0]['Results']));
+        // console.log('mera result : ' + JSON.stringify(result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0]['Results']));
           SourceDEFieldsResult = result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0]['Results'];
+        // console.log('my new result '+JSON.stringify(SourceDEFieldsResult));
         });
-
-    /*    for (var key in SourceDEFieldsResult) {
-          
-           DEListMap[SourceDEFieldsResult[key].DataExtension.CustomerKey] = {
-              "FieldName": SourceDEFieldsResult[key].Name,
-              "FieldIsRequired": SourceDEFieldsResult[key].IsRequired,
-              "FieldIsPrimaryKey": SourceDEFieldsResult[key].IsPrimaryKey,
-              "FieldFieldType": SourceDEFieldsResult[key].FieldType,
-              "FieldMaxLength": SourceDEFieldsResult[key].MaxLength,
-              "FieldScale": SourceDEFieldsResult[key].Scale,
-              "FieldDefaultValue": SourceDEFieldsResult[key].DefaultValue
+       for (var key in SourceDEFieldsResult) {
+       // DEListMap[SourceDEFieldsResult[key].DataExtension[0].CustomerKey[0]] = {
+        DEListMap= {
+          "FieldName": SourceDEFieldsResult[key].Name[0],
+          "CustomerKey":SourceDEFieldsResult[key].DataExtension[0].CustomerKey[0]
+       };
+        favorites.push(DEListMap);
+        
+        
+        
+        /*  if('MaxLength' in SourceDEFieldsResult[key] && 'Scale' in SourceDEFieldsResult[key] ) {
+            DEListMap[SourceDEFieldsResult[key].DataExtension[0].CustomerKey[0]] = {
+              "FieldName": SourceDEFieldsResult[key].Name[0],
+              "FieldIsRequired": SourceDEFieldsResult[key].IsRequired[0],
+              "FieldIsPrimaryKey": SourceDEFieldsResult[key].IsPrimaryKey[0],
+              "FieldFieldType": SourceDEFieldsResult[key].FieldType[0],
+              "FieldMaxLength": SourceDEFieldsResult[key].MaxLength[0],
+              "FieldScale": SourceDEFieldsResult[key].Scale[0],
+              "FieldDefaultValue": SourceDEFieldsResult[key].DefaultValue[0]
             };
+          }
+          else {
+            DEListMap[SourceDEFieldsResult[key].DataExtension[0].CustomerKey[0]] = {
+              "FieldName": SourceDEFieldsResult[key].Name[0],
+              "FieldIsRequired": SourceDEFieldsResult[key].IsRequired[0],
+              "FieldIsPrimaryKey": SourceDEFieldsResult[key].IsPrimaryKey[0],
+              "FieldFieldType": SourceDEFieldsResult[key].FieldType[0],
+              "FieldMaxLength": "",
+              "FieldScale": "",
+              "FieldDefaultValue": SourceDEFieldsResult[key].DefaultValue[0]
+            };
+          }*/
+           
           
         }
 
-        console.log("DEListMap" + DEListMap) ; 
-    */
-
+       console.log("DEListMap" + JSON.stringify(favorites)); 
+    
+        resCall.json({favorites : favorites});
 
 
    });
@@ -232,8 +257,8 @@ async function getacesstoken(ClientIdDestination,ClientSecretDestination,GrantTy
           .then( (response) => 
             { 
               var result = response.data; 
-              // console.log("Result"+result.access_token);
-              // console.log('Processing acess token'); 
+              //console.log("Result"+result.access_token);
+              //console.log('Processing acess token'); 
               resolve(result.access_token); 
             }, 
           (error) => 
@@ -249,7 +274,7 @@ async function getacesstoken(ClientIdDestination,ClientSecretDestination,GrantTy
 
 
 
-
+});
 
 
 
@@ -261,4 +286,3 @@ async function getacesstoken(ClientIdDestination,ClientSecretDestination,GrantTy
   // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running."));
-  
