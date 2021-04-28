672564e2cc0827334a8ad852f0b632143a43c683
01fazzy <faizal@cyntexa.com>
2021-04-28 16:43:10 +0530

    Faizal

--- app.js
+++ app.js
@@ -182,8 +182,8 @@ app.post("/secondpage", async function (req, res) {
 app.post("/RunQuery", async (reqCall,resCall)=>
 {
   console.log("Yeh hai Join Query Details --> " + JSON.stringify(reqCall)); 
-  var JoinQueryDetails = reqCall.body;
-  console.log('JoinQueryDetails : ' + reqCall.body);
+  var JoinQueryDetails = reqCall.body.JoinQueryDetails;
+  console.log('JoinQueryDetails : ' + reqCall.body.JoinQueryDetails);
 
 
 
