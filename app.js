4269b4fbdd8e1fa4baa104a80a88f146dcd1d7e6
01fazzy <faizal@cyntexa.com>
2021-07-12 14:46:40 +0530

    fazzy

--- app.js
+++ app.js
@@ -122,7 +122,6 @@ app.post("/secondpage", async function (req, res) {
             "FieldType": TempDEFieldsResult[key].FieldType[0]
           }));
         }
-        console.log('DEListMap : ' + JSON.stringify(DEListMap))
         var temp
         for (var val of Array.from(FieldSet)) {
           temp = JSON.parse(val);
@@ -134,50 +133,9 @@ app.post("/secondpage", async function (req, res) {
             });
           }
         }
+        console.log('DEListMap : ' + JSON.stringify(DEListMap))
         resCall.send(DEListMap);
       });
-    });
-  });
-
-
-  app.post("/fields", async (reqCall, resCall) => {
-    console.log("yeh app.post fields wale me hai token " + access_token);
-    var body2 = '<?xml version="1.0" encoding="UTF-8"?>\r\n<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">\r\n    <s:Header>\r\n        <a:Action s:mustUnderstand="1">Retrieve</a:Action>\r\n        <a:MessageID>urn:uuid:7e0cca04-57bd-4481-864c-6ea8039d2ea0</a:MessageID>\r\n        <a:ReplyTo>\r\n            <a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address>\r\n        </a:ReplyTo>\r\n        <a:To s:mustUnderstand="1">https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx</a:To>\r\n        <fueloauth xmlns="http://exacttarget.com">' + access_token + '</fueloauth>\r\n    </s:Header>\r\n    <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\r\n        <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI">\r\n            <RetrieveRequest>\r\n                <ObjectType>DataExtensionField</ObjectType>\r\n                <Properties>Client.ID</Properties>\r\n                <Properties>CreatedDate</Properties>\r\n                <Properties>CustomerKey</Properties>\r\n                <Properties>DataExtension.CustomerKey</Properties>\r\n                <Properties>DefaultValue</Properties>\r\n                <Properties>FieldType</Properties>\r\n                <Properties>IsPrimaryKey</Properties>\r\n                <Properties>IsRequired</Properties>\r\n                <Properties>MaxLength</Properties>\r\n                <Properties>ModifiedDate</Properties>\r\n                <Properties>Name</Properties>\r\n                <Properties>ObjectID</Properties>\r\n                <Properties>Ordinal</Properties>\r\n                <Properties>Scale</Properties>\r\n\r\n                               <QueryAllAccounts>true</QueryAllAccounts>\r\n                <Retrieves />\r\n                <Options>\r\n                    <SaveOptions />\r\n                    <IncludeObjects>true</IncludeObjects>\r\n                </Options>\r\n            </RetrieveRequest>\r\n      </RetrieveRequestMsg>\r\n   </s:Body>\r\n</s:Envelope>'
-    var options = {
-      'method': 'POST',
-      'url': 'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx',
-      'headers': {
-        'Content-Type': 'text/xml',
-        'SoapAction': 'Retrieve'
-      },
-      body: body2
-    };
-    request(options, function (error, response) {
-      //    if (error) throw new Error(error);
-      SourceDEFieldsResult = response.body;
-      xml2jsParser.parseString(SourceDEFieldsResult, function (err, result) {
-        // console.log('mera result : ' + JSON.stringify(result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0]['Results']));
-        SourceDEFieldsResult = result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0]['Results'];
-        // console.log('my new result '+JSON.stringify(SourceDEFieldsResult));
-      });
-
-
-      //code Faizal
-      var FieldSet = new Set();
-      for (var key in SourceDEFieldsResult) {
-        FieldSet.add(JSON.stringify({
-          "CustomerKey": SourceDEFieldsResult[key].DataExtension[0].CustomerKey[0],
-          "FieldName": SourceDEFieldsResult[key].Name[0],
-          "FieldType": SourceDEFieldsResult[key].FieldType[0]
-        }));
-      }
-      favorites = [];
-      for (var val of Array.from(FieldSet)) {
-        favorites.push(JSON.parse(val));
-      }
-      //code khatm
-
-      resCall.json({ favorites: favorites });
     });
   });
 
