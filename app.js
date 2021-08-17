ef513d27eff2570a0859ca4fb8bf67591fd4ee2e
ANIL KUMAR <anil@cyntexa.com>
2021-08-17 22:48:46 +0530

    Anil kumar

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.get("/", function (req, res) {
 
 app.set('view engine', 'html');
 
-var NewDEName;
 //Added By ANIL KUMAR
 app.post("/credential", async function (req, res) {
   console.log(req.body.clientid);
@@ -53,7 +52,7 @@ app.post("/credential", async function (req, res) {
   //  "ClientSecret" : "vhmEsBaxDl3LVeqYbLUxsg6p",
   //  "ClinentAuthURL" : "https://mc6vgk-sxj9p08pqwxqz9hw9-4my.auth.marketingcloudapis.com/"
   //}
-
+  var NewDEName;
   var AuthResponse = await getacesstoken(AuthRequest);
   console.log(AuthResponse);
   if(AuthResponse.AccessToken)
@@ -68,36 +67,7 @@ app.post("/credential", async function (req, res) {
   });
   }
   
-  async function getacesstoken(AuthRequest) {
-    try {
-      return new Promise(function (resolve, reject) {
-        axios.post( AuthRequest.ClinentAuthURL + 'v2/token',
-        {
-          'client_id': AuthRequest.ClientId,
-          'client_secret': AuthRequest.ClientSecret,
-          'grant_type': 'client_credentials'
-        })
-        .then((response) => {
-          resolve({
-              'AccessToken' : response.data.access_token,
-              'RestURL' : response.data.rest_instance_url,
-              'SoapURL' : response.data.soap_instance_url
-            });
-        },
-        (error) => {
-          //reject(error);
-          //res.end();
-        })
-
-      });
-    }
-    catch (err) { 
-    }
-  }
-
-});
-
-app.post("/DEListFetch", async (reqCall, resCall) => {
+  app.post("/DEListFetch", async (reqCall, resCall) => {
     DEListMap.DataViewMap = {
       "_EnterpriseAttribute": {
         "DEName": "_EnterpriseAttribute",
@@ -1890,7 +1860,7 @@ app.post("/DEListFetch", async (reqCall, resCall) => {
 
   });
 
-app.post("/RunQuery", async (reqCall, resCall) => {
+  app.post("/RunQuery", async (reqCall, resCall) => {
     var DERecords = [];
     DERecords = await getDERecords(NewDEName);
     resCall.send(DERecords);
@@ -1925,6 +1895,36 @@ app.post("/RunQuery", async (reqCall, resCall) => {
       })
     }
   })
+
+
+  async function getacesstoken(AuthRequest) {
+    try {
+      return new Promise(function (resolve, reject) {
+        axios.post( AuthRequest.ClinentAuthURL + 'v2/token',
+        {
+          'client_id': AuthRequest.ClientId,
+          'client_secret': AuthRequest.ClientSecret,
+          'grant_type': 'client_credentials'
+        })
+        .then((response) => {
+          resolve({
+              'AccessToken' : response.data.access_token,
+              'RestURL' : response.data.rest_instance_url,
+              'SoapURL' : response.data.soap_instance_url
+            });
+        },
+        (error) => {
+          //reject(error);
+          //res.end();
+        })
+
+      });
+    }
+    catch (err) { 
+    }
+  }
+
+});  
 
 
 app.listen(process.env.PORT || 3000,
