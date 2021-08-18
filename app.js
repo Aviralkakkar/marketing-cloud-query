abc9f66c64976c2caa1181dfdbbafeaa0e5aaeee
ANIL KUMAR <anil@cyntexa.com>
2021-08-18 11:09:53 +0530

    Anil kumar

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ app.set('view engine', 'html');
 
 app.post("/secondpage", async function (req, res) {
    var AuthRequest = {
-    "ClientId" : req.body.clientid,
-    "ClientSecret" : req.body.clientsecret,
-    "ClinentAuthURL" : req.body.authurl
+     "ClientId" : req.body.clientid,
+     "ClientSecret" : req.body.clientsecret,
+     "ClinentAuthURL" : req.body.authurl
    }
   //var AuthRequest = {
   //  "ClientId" : "sr7id7zht854bwdco8t9qdym",
@@ -53,15 +53,14 @@ app.post("/secondpage", async function (req, res) {
   if(AuthResponse.AccessToken)
   {
     console.log('Successfully redirected');
-    console.log('URL:'+req.url);
-    res.sendFile(path.join(__dirname + '/public/secondpage.html'));
-    //res.writeHead(301, { "Location": "http://" + req.headers['host'] + '/secondpage.html' });
-    //    return res.end();
-    //res.redirect("/public/secondpage.html");
-    
-    
+    res.sendFile(path.join(__dirname + '/public/secondpage.html')); 
+  }
+  else
+  {
+    console.log('Something went wrong!');
   }
   
+
   app.post("/DEListFetch", async (reqCall, resCall) => {
     DEListMap.DataViewMap = {
       "_EnterpriseAttribute": {
